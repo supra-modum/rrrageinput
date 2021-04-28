d876dcbf3cee0bdce6ed170ed57a2e8eb2d8f3a3
Екатерина Заславская <3mfhognm5@relay.firefox.com>
2021-04-28 21:34:09 +0300

    RRR-main: initial commit

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import '../styles/tailwind.css'
+import { App } from "./App";
+
+ReactDOM.render(
+  <App userName="rrrangeinput" />,
+  document.getElementById("root")
+);
