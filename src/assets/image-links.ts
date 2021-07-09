32da16dbf2f5c38a8a003f3db7f927abfdf5aec4
Екатерина Заславская <catherine.zaslavsckaya@yandex.ru>
2021-07-10 02:02:34 +0300

    change favicon

--- src/assets/image-links.ts
+++
@@ -1,2 +0,0 @@
-export const imageHeroMain =
-  'https://images.unsplash.com/photo-1517134191118-9d595e4c8c2b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80';
