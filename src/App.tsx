796f09e685881b794642428742a9eceabd747749
Kateryna Zaslavsky <rginput@protonmail.com>
2022-10-21 00:51:20 +0400

    update main page and deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+// import { Switch, Route, Redirect } from 'react-router-dom';
 import 'cirrus-ui';
 
-import { Home } from './home';
-import { ProjectsAndBlog } from './projects-blog';
-import { textMainPageEng } from './model';
+// import { Home } from './home';
+// import { ProjectsAndBlog } from './projects-blog';
+// import { textMainPageEng } from './model';
 
 export const App: React.FC = () => {
   return (
     <div className='hero fullscreen bg-yellow-300'>
       <div className='hero-body'>
         <div className='mx-auto'>
-          <h1 className='title uppercase'>
-            <a href="https://savelife.in.ua/en/donate-en/" className='text-blue-600'>savelife.in.ua/donate</a>
+          <h1 className='title'>
+            <a
+              href='https://savelife.in.ua/en/donate-en/'
+              className='text-blue-600'
+            >
+              savelife.in.ua
+            </a>
           </h1>
         </div>
       </div>
